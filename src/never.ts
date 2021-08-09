8378e4b2d31cf14708f2039fa8ca1f5ad32b765f
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-08-09 13:26:00 +0900

    learn never

---
+++ src/never.ts
@@ -0,0 +1,16 @@
+export { };
+
+const error = (message: string): never => {
+  throw new Error(message);
+}
+
+try {
+  error('test');
+} catch (error) {
+  console.log({ error });
+}
+
+let foo: void = undefined;
+
+// let bar: never = undefined;
+let bar: never = error('only me');
