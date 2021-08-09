170ee88c23f6779c6bc2ca19e0dc7fa9b84b5ff3
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-08-09 14:57:06 +0900

    learn literal

---
+++ src/literal.ts
@@ -0,0 +1,13 @@
+export { };
+
+let dayOfTheWeek: '日' | '月' | '火' | '水' | '木' | '金' | '土';
+dayOfTheWeek = '月';
+// dayOfTheWeek = '31';
+
+let month: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+month = 12;
+// month = 13;
+
+let TRUE: true;
+TRUE = true;
+// TRUE = false;
