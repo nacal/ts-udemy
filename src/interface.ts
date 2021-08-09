cd353cf22dec0f7911c55c6991b7f8b3237da17c
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-08-09 14:17:56 +0900

    learn interface

---
+++ src/interface.ts
@@ -0,0 +1,23 @@
+export { };
+
+//Type Ailiases
+type ObjectType = {
+  name: string;
+  age: number;
+};
+
+let object: ObjectType = {
+  name: 'Ham',
+  age: 24
+}
+
+//Interface
+interface ObjectInterface {
+  name: string;
+  age: number;
+}
+
+let object2: ObjectInterface = {
+  name: 'Ham',
+  age: 24
+}
