9e6a8f0ec74031dd6f0dfa528ab0f2c9575f7f5a
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-08-19 00:16:35 +0900

    learn overloads(arrow)

--- src/overloads.ts
+++ src/overloads.ts
@@ -9,3 +9,16 @@ function double(value: any): any {
 
 console.log(double(10))
 console.log(double('aa'))
+
+
+interface Double {
+  (value: number): number;
+  (value: string): string;
+}
+
+const double2: Double = (value: any): any => {
+  return (typeof value === 'number') ? value * 2 : value + value
+}
+
+console.log(double2(10))
+console.log(double2('aa'))
