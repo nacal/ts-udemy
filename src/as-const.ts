2cb44e783af2a0f3ffd7116d96fe452d8c9b4f77
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-08-24 15:29:21 +0900

    learn const assertions

---
+++ src/as-const.ts
@@ -0,0 +1,17 @@
+export { };
+
+
+let name = 'Taro';
+
+name = 'Jiro';
+
+let nickname = 'Hanako' as const;
+// nickname = 'Hanakon'
+
+let profile = {
+  name: 'Taro',
+  height: 180
+} as const;
+
+// profile.name = 'jiro'
+// profile.height = 190
