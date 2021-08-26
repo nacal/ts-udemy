493323a58dbc5f2016c946c81a13482a605dab3a
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-08-26 16:13:24 +0900

    learn pick and omit

---
+++ src/section6/pick-and-omit.ts
@@ -0,0 +1,10 @@
+export { };
+
+interface DetailedProfile {
+  name: string;
+  height: number;
+  weight: number;
+}
+
+type SimpleProfile = Pick<DetailedProfile, 'name' | 'weight'>;
+type SmallProfile = Omit<DetailedProfile, 'height'>;
